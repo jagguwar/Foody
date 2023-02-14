b7319fadf029d00b5ecedd8a480e5bdef643e3f1
jagguwar <dnhtrk@hotmail.com>
2023-02-14 16:20:45 -0300

    Recipes and Placeholder Row Layouts designed, refactored some files

--- app/src/main/java/com/app/foody/FavoriteRecipesFragment.kt
+++ app/src/main/java/com/app/foody/FavoriteRecipesFragment.kt
@@ -6,14 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-class foodJokeFragment : Fragment() {
+class FavoriteRecipesFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_food_joke, container, false)
+        return inflater.inflate(R.layout.fragment_favorite_recipes, container, false)
     }
 
 }
--- app/src/main/java/com/app/foody/FoodJokeFragment.kt
+++ app/src/main/java/com/app/foody/FoodJokeFragment.kt
@@ -6,14 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-class recipesFragment : Fragment() {
+class FoodJokeFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recipes, container, false)
+        return inflater.inflate(R.layout.fragment_food_joke, container, false)
     }
 
 }
