13aee1c98f635b45a3489919a3cf564f4014df0e
jagguwar <dnhtrk@hotmail.com>
2023-02-13 21:45:11 -0300

    Setup Navigation Component

--- app/src/main/java/com/app/foody/MainActivity.kt
+++ app/src/main/java/com/app/foody/MainActivity.kt
@@ -2,10 +2,32 @@ package com.app.foody
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.ui.AppBarConfiguration
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    navController = findNavController(R.id.nav_host_fragment)
+    val appBarConfiguration = AppBarConfiguration(
+        setOf(
+            R.id.recipesFragment,
+            R.id.favoriteRecipesFragment,
+            R.id.foodJokeFragment
+        )
+    )
+
+    bottomNavigationView.setupWithNavController(navController)
+    setupActionBarWithNavController(navController, appBarConfiguration)
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
 }
