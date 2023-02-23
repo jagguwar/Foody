5a9132826319374a1365f59910b2f2e48e32d9df
jagguwar <dnhtrk@hotmail.com>
2023-02-22 23:25:19 -0300

    Created RecipesEntity and RecipesDao

---
+++ app/src/main/java/com/app/foody/RecipesEntity.kt
@@ -0,0 +1,14 @@
+package com.app.foody
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.app.foody.models.FoodRecipe
+import com.app.foody.util.Constants.Companion.RECIPES_TABLE
+
+@Entity(tableName = RECIPES_TABLE)
+class RecipesEntity(var foodRecipe: FoodRecipe) {
+
+    @PrimaryKey(autoGenerate = false)
+    var id: Int = 0
+
+}
