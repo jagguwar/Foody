750f8c814f0498281fbe647e429bc4a909fbdf05
jagguwar <dnhtrk@hotmail.com>
2023-02-26 21:34:59 -0300

    Designed RecipesBottomSheet layout, set navigation from RecipesFragment to RecipesBottomSheet fragment dialog

--- app/src/main/java/com/app/foody/ui/fragments/recipes/RecipesFragment.kt
+++ app/src/main/java/com/app/foody/ui/fragments/recipes/RecipesFragment.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.app.foody.viewmodels.MainViewModel
 import com.app.foody.R
@@ -46,6 +47,10 @@ class RecipesFragment : Fragment() {
 
         setupRecyclerView()
         readDatabase()
+
+        binding.recipesFab.setOnClickListener {
+            findNavController().navigate(R.id.action_recipesFragment_to_recipesBottomSheet)
+        }
 
         return binding.root
     }
