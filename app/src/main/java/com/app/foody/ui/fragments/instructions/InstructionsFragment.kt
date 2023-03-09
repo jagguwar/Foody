5b1c0f5ad2da90c15e3e4066a270d3cb5296c338
jagguwar <dnhtrk@hotmail.com>
2023-03-08 21:27:29 -0300

    Modified InstructionsFragment, fixed some layout details on recipes_row_layout.xml

--- app/src/main/java/com/app/foody/ui/fragments/instructions/InstructionsFragment.kt
+++ app/src/main/java/com/app/foody/ui/fragments/instructions/InstructionsFragment.kt
@@ -5,7 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebViewClient
 import com.app.foody.R
+import com.app.foody.models.Result
+import com.app.foody.util.Constants
+import kotlinx.android.synthetic.main.fragment_instructions.view.*
 
 class InstructionsFragment : Fragment() {
 
@@ -13,8 +17,17 @@ class InstructionsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_instructions, container, false)
+
+        val view = inflater.inflate(R.layout.fragment_instructions, container, false)
+
+        val args = arguments
+        val myBundle: Result? = args?.getParcelable(Constants.RECIPE_RESULT_KEY)
+
+        view.instructions_webView.webViewClient = object : WebViewClient() {}
+        val websiteUrl: String = myBundle!!.sourceUrl
+        view.instructions_webView.loadUrl(websiteUrl)
+
+        return view
     }
 
 }
