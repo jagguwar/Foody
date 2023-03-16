b5f117965bd83264bade019cef629bd754badaf0
jagguwar <dnhtrk@hotmail.com>
2023-03-16 17:50:59 -0300

    Added a Splash Screen

--- app/src/main/java/com/app/foody/ui/MainActivity.kt
+++ app/src/main/java/com/app/foody/ui/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_Foody)
         setContentView(R.layout.activity_main)
     }
 
